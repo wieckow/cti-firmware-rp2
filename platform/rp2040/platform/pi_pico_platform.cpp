2ebbd48ed49d45d074aa1fc1dffb06da5b78c9db
wieckow <47519516+wieckow@users.noreply.github.com>
2024-11-12 09:57:35 +0100

    Add files via upload

---
+++ platform/rp2040/platform/pi_pico_platform.cpp
@@ -0,0 +1,35 @@
+#include "cti/platform.h"
+
+#include <pico/stdlib.h>
+#include <pico/unique_id.h>
+#include "hardware/adc.h"
+
+#include <cstring>
+
+using namespace CTI;
+
+const uint32_t unique_id_len = PICO_UNIQUE_BOARD_ID_SIZE_BYTES * 2 + 1;
+char unique_id[unique_id_len] = "0000000000000000";
+
+void Platform::Preinit() {
+    pico_get_unique_board_id_string(unique_id, unique_id_len);
+
+    stdio_usb_init();
+
+    bool status = false;
+    
+    //need to give time for USB init and enumeration
+    for (int i = 0; i < 12; ++i) {
+        sleep_ms(400);
+        status = !status;
+        gPlatform.IO.StatusLED(status);
+    }
+}
+
+void Platform::Init() {
+    adc_init();
+    adc_set_temp_sensor_enabled(true);
+}
+
+const char* PlatformInfo::Model() const { return "RP2350-Visa"; };
+const char* PlatformInfo::SerialNum() const { return unique_id; };
